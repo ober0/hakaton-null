0599e5b47d0c9e71cfaf9d726bbc7e3ce91289b7
Ruslan <136484210+ober0@users.noreply.github.com>
2024-11-12 12:30:45 +0300

    рабочее api Для получения и сохранения влажности и шума

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 import json
-from .models import Temperature, Noice, Humidity
+from .models import Temperature, Noice, Humidity, PeopleData
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -10,7 +10,7 @@ def setTemperature(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            temperature = data['temperature']
+            temperature_val = data['temperature']
             place = data['place']
         except Exception as e:
             return JsonResponse({
@@ -19,7 +19,7 @@ def setTemperature(request):
             })
 
         try:
-            temperature = Temperature(place=place, temperature=temperature)
+            temperature = Temperature(place=place, temperature=temperature_val)
             temperature.save()
             return JsonResponse({'success': True})
         except Exception as e:
@@ -30,12 +30,58 @@ def setTemperature(request):
 
 
 def setHumidity(request):
-    pass
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            humidity_val = data['humidity']
+            place = data['place']
+        except Exception as e:
+            return JsonResponse({
+                'success': False,
+                'error': str(e)
+            })
+
+        try:
+            humidity = Humidity(place=place, humidity=humidity_val)
+            humidity.save()
+            return JsonResponse({'success': True})
+        except Exception as e:
+            return JsonResponse({
+                'success': False,
+                'error': str(e)
+            })
 
 
 def setNoice(request):
-    pass
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            place = data['place']
+            noice_val = data['noice']
+        except Exception as e:
+            return JsonResponse({
+                'success': False,
+                'error': str(e)
+            })
+
+        try:
+            noice = Temperature(place=place, noice=noice_val)
+            noice.save()
+            return JsonResponse({'success': True})
+        except Exception as e:
+            return JsonResponse({
+                'success': False,
+                'error': str(e)
+            })
 
 
 def setPeopleData(request):
-    pass
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            place = data['place']
+        except Exception as e:
+            return JsonResponse({
+                'success': False,
+                'error': str(e)
+            })
